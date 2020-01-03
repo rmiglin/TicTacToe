fc69da9b88a2e8721a357c879624721738d6349d
Ross Miglin <rossmiglin@gmail.com>
2020-01-03 01:21:29 -0500

    added text to show X or O turn

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -10,8 +10,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Tic-Tac-Toe")
+        VStack{
+            Spacer()
+            HStack{
+                Text("Your turn:")
+                Text("❌⭕️")
+            }.padding(.bottom, 20)
+            HStack{
+                Spacer()
+                //Image("Background").Scale.medium
+                //Rectangle().fill()
+                Spacer()
+            }
+            .padding(.bottom, 200)
+            //let image1 = Image("Background").Scale.medium
+            
+            
+            //View()
+            //Image("Background")
+            
+            Spacer()
+            //Text("Tic-Tac-Toe")
+        }//.navigationBarTitle("Tic-Tac-Toe")
     }
+}
+
+ func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+    let size = image.size
+
+    let widthRatio  = targetSize.width  / size.width
+    let heightRatio = targetSize.height / size.height
+
+    // Figure out what our orientation is, and use that to form the rectangle
+    var newSize: CGSize
+    if(widthRatio > heightRatio) {
+        newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+    } else {
+        newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+    }
+
+    // This is the rect that we've calculated out and this is what is actually used below
+    let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+
+    // Actually do the resizing to the rect using the ImageContext stuff
+    UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+    image.draw(in: rect)
+    let newImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+
+    return newImage!
 }
 
 struct ContentView_Previews: PreviewProvider {
