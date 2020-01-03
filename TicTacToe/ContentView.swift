735fd9d0533d63524257eb3315584e5984c80203
Ross Miglin <rossmiglin@gmail.com>
2020-01-02 21:21:12 -0500

    Initial commit

---
+++ TicTacToe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by Ross Miglin on 1/2/20.
+//  Copyright © 2020 Ross Miglin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Tic-Tac-Toe")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
